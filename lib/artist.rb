078bb1d389ee30ecdab9f0df419577a66611a21a
Ali Karimi <ajkarimi7@gmail.com>
2019-09-19 19:50:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,6 +20,23 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end 
   
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end 
+  
+  def self.find(names)
+    self.all.find {|artist| artist.name == name }
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def print_songs
+    songs.each {|song| puts song.name}
+  end 
+  
+end 
   
   
   
