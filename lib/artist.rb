65448886bd479b53d210532428df176523c22122
Ali Karimi <ajkarimi7@gmail.com>
2019-09-19 19:46:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,26 @@
 class Artist
   
+  @@all = []
+  attr_accessor :name, :songs 
+  
+  def initialize(name)
+    @name = name
+    save
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def add_song(songs)
+    song.artist = self
+  end 
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end 
+  
+  
+  
   
 end 
